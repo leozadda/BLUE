c76087c0a2f840f5cbe8e7a56bf27e9083e2acb7
Leo <leo@Leos-MacBook-Pro.local>
2025-01-20 21:52:57 -0600

    I hard coded the api url for front end components as https://api.b-lu-e.com

--- frontend/src/login/LogIn.js
+++ frontend/src/login/LogIn.js
@@ -26,7 +26,7 @@ const LogIn = () => {
 
     try {
       // Send login request to the server
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+      const response = await fetch(`https://api.b-lu-e.com/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
